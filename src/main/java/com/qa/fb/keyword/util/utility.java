fe0dd42bf78208090a53621d1cac6f3caffd6055
aamir0143 <mdaamirreza00@gmail.com>
2022-08-17 00:33:54 +0530

    KeyWordDrivenFrameWork

---
+++ src/main/java/com/qa/fb/keyword/util/utility.java
@@ -0,0 +1,19 @@
+package com.qa.fb.keyword.util;
+
+
+
+import com.qa.fb.keyword.base.Base;
+
+public class utility extends Base{
+	
+	public static long PAGE_LOAD_TIME = 30;
+
+	public static long IMPLICIT_WAIT_TIME = 20;
+	
+
+	
+}
+
+
+
+	
